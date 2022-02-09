03c6f94110a499998724b2f5af431f992f5f9b16
Akshay <alamba093@gmail.com>
2022-02-10 03:57:19 +0400

    bugs fixes

--- components/listItem.js
+++ components/listItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Image, View, TouchableOpacity } from 'react-native'
 import { Text, Avatar, withStyles, List } from 'react-native-ui-kitten'
 
+import { Icon } from 'react-native-ui-kitten'
 
 const listItem = ({ item }) => (
     <View style={{
@@ -9,7 +10,7 @@ const listItem = ({ item }) => (
         marginBottom: 25
       }}>
     <Image
-      source={{ uri: item.imageURI }}
+      source={{ uri: item.largeImageURL }}
       style={{
         width: '100%',
         height: 300
@@ -21,29 +22,65 @@ const listItem = ({ item }) => (
     alignItems: 'center',
     justifyContent: 'space-between'
   }}>
-      <Text category='s1' style={{
-    color: 'black'
+   <View style={{
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
   }}>
-        {item.postTitle}
-      </Text>
-      <TouchableOpacity
-        onPress={()=>{}}>
-        <Avatar
-          source={{ uri: item.avatarURI }}
+   <Avatar
+          source={{ uri: item.userImageURL }}
           size='small'
           style={ {
             marginRight: 16
           }}
         />
-      </TouchableOpacity>
-    </View>
-    <View style={{
-    padding: 10,
-    borderWidth: 0.25,
-    borderColor: 'white'
+      <Text category='s1' style={{
+    color: 'black'
   }}>
-      <Text category='p2'>{item.randomText}</Text>
+        {item.user}
+      </Text>
+
+
+       
+       
     </View>
+<View style={{
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  }}>
+    <Icon
+            name='heart-outline'
+            width={25}
+            height={25}
+            fill='#939393'
+          />
+          <Text style={{
+            marginRight:10
+          }}>{item.likes}</Text>
+          
+          <Icon
+            name='download-outline'
+            width={25}
+            height={25}
+            fill='#939393'
+          />
+          <Text>{item.downloads}</Text>
+          </View>
+    </View>
+        <View  style={{
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  }}>
+        <Text>Tags: {item.tags.split(",").map((e) => {
+         return (   <Text>{e}</Text>)
+        })}</Text>
+        </View>
+    
   </View>
   )
 
