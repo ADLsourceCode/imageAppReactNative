3e584dcff9dc063aa1003d0048436073d334d5dd
Akshay <alamba093@gmail.com>
2022-02-10 03:55:48 +0400

    data fetching , infinite scrolling , UI framework

--- screens/feed.js
+++ screens/feed.js
@@ -1,40 +1,94 @@
 import React, { Component } from 'react'
-import { Image, View, TouchableOpacity } from 'react-native'
+import { Image, View, TouchableOpacity,ActivityIndicator,RefreshControl } from 'react-native'
+import { FlatList, } from 'react-navigation';
 import { Text, Avatar, withStyles, List } from 'react-native-ui-kitten'
 import ListItem from '../components/listItem';
 
-const DATA = [
-    {
-      id: 1,
-      postTitle: 'Planet of Nature',
-      avatarURI:
-        'https://images.unsplash.com/photo-1559526323-cb2f2fe2591b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-      imageURI:
-        'https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-      randomText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
-    },
-    {
-      id: 2,
-      postTitle: 'Lampost',
-      avatarURI:
-        'https://images.unsplash.com/photo-1559526323-cb2f2fe2591b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-      imageURI:
-        'https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-      randomText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
-    }
-  ]
 
-const Feed = () => (
- <List
-    style={{
-        flex: 1
-      }}
-    data={DATA}
-    renderItem={ListItem}
-    keyExtractor={DATA.id}
-  />
-)
+
+  class Feed extends Component {
+    state = { DATA: null, isRefreshing: false,page:1,perPage:5, loadingMore: false, }
+    
+
+    componentDidMount() {
+      this.fetchPosts()
+    }
+
+
+    fetchPosts = async () => {
+      try {
+        const { page } = this.state;
+        var URL  = `https://pixabay.com/api/?key=25635193-22a1499fce771e0aa36ed3765&q=yellow+flowers&image_type=photo&pretty=true&page=${page}&per_page=5`;
+        let response = await fetch(URL);
+        let responseJson = await response.json();
+       // console.log(responseJson)
+       
+        this.setState({ DATA:
+          page === 1
+              ? responseJson["hits"]
+              : [...this.state.DATA, ...responseJson["hits"]]
+          
+        
+        , isRefreshing: false })
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    onRefresh = () => {
+      this.setState({ isRefreshing: true })
+      this.fetchPosts()
+    }
+
+    _loadMore= () => {
+      this.setState(
+        (prevState, nextProps) => ({
+          page: prevState.page + 1,
+          loadingMore: true
+        }),
+        () => {
+          this.fetchPosts();
+        }
+      );
+    }
+
+    
+
+   
+    render () {
+      
+      if (this.state.DATA != null) {
+      return (
+        
+        <FlatList
+        style={{
+            flex: 1
+          }}
+        renderItem={ListItem}
+        // key
+        data={this.state.DATA}
+        keyExtractor={this.state.DATA.id}
+        refreshControl={<RefreshControl refreshing={this.state.refreshing}
+         onRefresh={this.onRefresh} />}
+         onEndReached={this._loadMore}
+        onEndReachedThreshold={0.5}
+        initialNumToRender={10}
+        // ListFooterComponent={this._Footer}
+      />
+      )
+    } else{
+    return (
+      <View
+        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator size='large' />
+      </View>
+    )
+    }
+    
+
+    }
+
+  }
+
 export default Feed
 
