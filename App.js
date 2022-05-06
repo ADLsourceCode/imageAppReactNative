222550bfa6d03a68e3036ca359f1516415eec26a
Akshay <alamba093@gmail.com>
2022-05-06 16:46:28 +0400

    fixes

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { mapping, light as lightTheme } from '@eva-design/eva'
 import { ApplicationProvider, IconRegistry } from 'react-native-ui-kitten'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
 
-import TabNavigator from './Misc/tabNavigator'
+import TabNavigator from './misc/tabNavigator'
 
 export default function App() {
   return (
